941d5643a0b0e1f7dac1f3183426b67a861b0e45
darty <darty@192.168.2.11>
2014-01-20 14:55:47 -0500

    code changes

--- interviews/src/main/java/com/triton/interviews/LamaWoolService.java
+++ interviews/src/main/java/com/triton/interviews/LamaWoolService.java
@@ -6,10 +6,7 @@ import org.joda.time.MutableDateTime;
 public class LamaWoolService {
 	
 	public boolean isWoolTooOldToSell(DateTime shearDate) {
-		// Joda mutable DateTime instance initialized to the current system date/time
-		//you can't use au current date time, or change the current date so that it can match the test
-		//otherwise the result will be random with respect to the current date
-		
+		// Joda mutable DateTime instance initialized to the current system date/time		
 		MutableDateTime now = shearDate.toMutableDateTime();
 		now.setDate(shearDate); 
 		
